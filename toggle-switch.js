57d5550d378fb43c7e9ca2e398e41ece7441c369
Stephen Radford <steve228uk@gmail.com>
2012-11-18 17:58:16 +0000

    Fixed out of scope and switched to 'prop' to stop some issues'

--- toggle-switch.js
+++ toggle-switch.js
@@ -7,22 +7,25 @@ URL:        http://github.com/jamiebicknell/Toggle-Switch
 $(document).ready(function () {
     $('.switch, label.switch-label').each(function(i) {
         $(this).click(function(e) {
+
+            var $value, $check;
+
             e.preventDefault();
             if($(this).is('label')) {
-                var $value = $('#'+$(this).attr('for')).prev('span.switched');
-                var $check = $('#'+$(this).attr('for'));
+                $value = $('#'+$(this).attr('for')).prev('span.switched');
+                $check = $('#'+$(this).attr('for'));
             }
             else {
-                var $value = $(this).children('span.switched');
-                var $check = $(this).children('input[type=checkbox]');
+                $value = $(this).children('span.switched');
+                $check = $(this).children('input[type=checkbox]');
             }
             if($value.is('.off')) {
                 $value.stop().animate({left: 0},150).removeClass('off');
-                $check.attr('checked','checked');
+                $check.prop('checked','checked');
             }
             else {
                 $value.stop().animate({left: -21},150).addClass('off');
-                $check.attr('checked','');
+                $check.prop('checked','');
             }
         });
     });
